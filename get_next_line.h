5e9a9bff43d20c7495c57b9689b6e8687f42a11d
tdieumeg <tdieumeg@student.42.fr>
2015-12-10 16:13:10 +0100

    ok

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdieumeg <tdieumeg@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/02/05 12:42:10 by tdieumeg          #+#    #+#             */
+/*   Updated: 2014/03/27 15:09:39 by tdieumeg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 512
+
+int				get_next_line(int const fd, char **line);
+
+#endif
